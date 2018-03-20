c2a45e1e38457f7252db8a4775d0f4f81dbfd3c6
Alan <akostrick@hotmail.com>
2018-03-20 15:16:05 -0400

    adding repo to BrowseController

--- src/main/java/org/wecancodeit/ecom/catalog/BrowseController.java
+++ src/main/java/org/wecancodeit/ecom/catalog/BrowseController.java
@@ -3,6 +3,7 @@ package org.wecancodeit.ecom.catalog;
 import java.util.Collection;
 import java.util.Collections;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BrowseController {
 
+	private CrudRepository<Product, Long> productRepo;
+	
 	@RequestMapping("/products")
-	public Collection<Product> getProducts() {
-		return Collections.singleton(new Product("arbitrary product name"));
+	public Iterable<Product> getProducts() {
+		return productRepo.findAll();
 	}
 
 	@RequestMapping("/products/{id}")
--- src/test/java/org/wecancodeit/ecom/catalog/BrowseControllerTest.java
+++ src/test/java/org/wecancodeit/ecom/catalog/BrowseControllerTest.java
@@ -1,31 +1,58 @@
 package org.wecancodeit.ecom.catalog;
 
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.any;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
 
-import java.util.Collection;
+import java.util.Collections;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.repository.CrudRepository;
 
 public class BrowseControllerTest {
 
-	@Test
-	public void shouldGetProducts() {
-		BrowseController underTest = new BrowseController();
-		
-		Collection<Product> result = underTest.getProducts();
-		
-		assertThat(result, hasSize(greaterThan(0)));
+	@InjectMocks
+	private BrowseController underTest;
+	
+	@Mock
+	private CrudRepository<Product, Long> productRepo;
+	
+	@Mock
+	private Product product;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
 	}
 	
 	@Test
-	public void shouldGeAnIndividualProduct() {
-		BrowseController underTest = new BrowseController();
+	public void shouldGetProducts() {
+		when(productRepo.findAll()).thenReturn(Collections.singleton(product));
+
+		Iterable<Product> result = underTest.getProducts();
 		
+		assertThat(result, contains(any(Product.class)));
+	}
+	
+	@Test
+	public void shouldGetProductsFromDb() {
+		when(productRepo.findAll()).thenReturn(Collections.singleton(product));
+
+		Iterable<Product> result = underTest.getProducts();
+		
+		assertThat(result, contains(product));
+	}
+	
+	@Test
+	public void shouldGetAnIndividualProduct() {
 		Product result = underTest.getProduct(42L);
 		
 		assertThat(result, is(not(nullValue())));
