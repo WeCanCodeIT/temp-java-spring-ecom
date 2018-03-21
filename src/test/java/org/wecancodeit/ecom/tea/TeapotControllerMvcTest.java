dce7de62ba2170ce0d78add21d77c414e8991a5c
Alan <akostrick@hotmail.com>
2018-03-21 14:01:50 -0400

    teapots do not make coffee

---
+++ src/test/java/org/wecancodeit/ecom/tea/TeapotControllerMvcTest.java
@@ -0,0 +1,26 @@
+package org.wecancodeit.ecom.tea;
+
+import static org.springframework.http.HttpStatus.I_AM_A_TEAPOT;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(TeapotController.class)
+public class TeapotControllerMvcTest {
+
+	@Resource
+	private MockMvc mvc;
+
+	@Test
+	public void shouldNotMakeCoffee() throws Exception {
+		mvc.perform(get("/teapot/coffee")).andExpect(status().is(I_AM_A_TEAPOT.value()));
+	}
+}
